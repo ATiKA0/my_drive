e66182702f6bf626600c5f561c661b8cabfe3496
ATiKA0 <gluckati78@gmail.com>
2023-05-11 10:18:28 +0200

    mode change menu for list working icons in list, favorite star added to list added file info panel

--- api.php
+++ api.php
@@ -5,21 +5,21 @@ require 'functions.php';
 $info = [];
 $info['succes'] = false;
 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['data_type'])){
-    
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['data_type'])) {
+
     $info['data_type'] = $_POST['data_type'];
 
-    if($_POST['data_type'] == 'upload_files'){
+    if ($_POST['data_type'] == 'upload_files') {
         $folder = 'uploads/';
-        if(!file_exists($folder)){
-            mkdir($folder, 0777,true);
-            file_put_contents($folder.".htacces", "Options -Indexes");
+        if (!file_exists($folder)) {
+            mkdir($folder, 0777, true);
+            file_put_contents($folder . ".htacces", "Options -Indexes");
         }
         foreach ($_FILES as $key => $file) {
-            $destination = $folder.time().$file['name'];
-            if(file_exists($destination))
-                $destination = $folder.time().rand(0,9999).$file['name'];
-            move_uploaded_file($file['tmp_name'],$destination);
+            $destination = $folder . time() . $file['name'];
+            if (file_exists($destination))
+                $destination = $folder . time() . rand(0, 9999) . $file['name'];
+            move_uploaded_file($file['tmp_name'], $destination);
 
             //save to database
             $type = $file['type'];
@@ -35,16 +35,51 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['data_type'])){
             query($query);
             $info['succes'] = true;
         }
-    }else
-    if($_POST['data_type'] == 'get_files'){
+    } else
+    if ($_POST['data_type'] == 'get_files') {
 
-            $query = "SELECT * FROM mydrive order by id desc limit 30";
-            $rows = query($query);
-            
-            if($rows){
-               $info['rows'] = $rows; 
-               $info['succes'] = true; 
-            }   
+        $mode = $_POST['mode'];
+
+        switch ($mode) {
+            case 'MY DRIVE':
+                $query = "SELECT * FROM mydrive ORDER BY id DESC LIMIT 30";
+                break;
+
+            case 'FAVORITES':
+                $query = "SELECT * FROM mydrive WHERE favorite = 1 ORDER BY id DESC LIMIT 30";
+                break;
+
+            case 'RECENT':
+                $query = "SELECT * FROM mydrive ORDER BY date_updated DESC LIMIT 30";
+                break;
+
+            case 'TRASH':
+                $query = "SELECT * FROM mydrive WHERE trash = 1 ORDER BY id DESC LIMIT 30";
+                break;
+
+            default:
+                $query = "SELECT * FROM mydrive ORDER BY id DESC LIMIT 30";
+                break;
+        }
+
+        $rows = query($query);
+
+        if ($rows) {
+
+            foreach($rows as $key => $row){
+                $rows[$key]['icon'] = $icons[$row['file_type']] ?? '<i class="bi bi-question-square-fill class_39"></i>';
+                $rows[$key]['date_created'] = get_date($row['date_created']);
+                $rows[$key]['date_updated'] = get_date($row['date_updated']);
+                $rows[$key]['file_size'] = round($row['file_size'] / (1024 * 1024)) . " Mb";
+
+                if($rows[$key]['file_size'] == '0 Mb'){
+                    $rows[$key]['file_size'] = round($row['file_size'] / 1024) . " Kb";
+
+                }
+            }
+            $info['rows'] = $rows;
+            $info['succes'] = true;
+        }
     }
 }
 
